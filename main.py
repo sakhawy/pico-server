cb7b672575175726759441f1b20b0d86d7c346dd
Mohammad Alsakhawy <mohammadalsakhawy@gmail.com>
2023-11-13 04:32:26 +0200

    feat: replace TCPServer with PicoTCPServer

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 #! /usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+from __future__ import annotations
+
+import socket
 import http.server
 import socketserver
 import logging
@@ -19,7 +22,36 @@ logger = logging.getLogger(
     'http-server'
 )
 
-handler = PicoHTTPRequestHandler
-with socketserver.TCPServer(('0.0.0.0', 8000), handler) as http:
+
+class PicoTCPServer:
+    def __init__(
+        self, 
+        socket_address: tuple[str, int], 
+        request_handler: http.server.SimpleHTTPRequestHandler
+    ) -> None:
+        self.request_handler = request_handler
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.sock.bind(socket_address)
+
+        self.sock.listen()
+
+    def serve_forever(self) -> None:
+        while True:
+            conn, addr = self.sock.accept()
+
+            with conn:
+                logger.info(f'Accepted connection from {addr}')
+                self.request_handler(conn, addr, self.sock)
+            logger.info(f'Closed connection from {addr}')
+
+    def __enter__(self) -> PicoTCPServer:
+        return self
+
+    def __exit__(self, *args) -> None:
+        self.sock.close()
+
+
+with PicoTCPServer(('0.0.0.0', 8000), http.server.SimpleHTTPRequestHandler) as http:
     logger.info(f'Running on port 8000')
     http.serve_forever()
--- pico_http_request_handler.py
+++ pico_http_request_handler.py
@@ -1,11 +1,24 @@
 import shutil
 import socketserver
 import socket
+import logging
+
+
+logger = logging.getLogger(
+    'http-server'
+)
+
 
 class PicoHTTPRequestHandler(socketserver.BaseRequestHandler):
     '''
     Servers HTTP files.
     '''
+    def __init__(self, request, client_address, server) -> None:
+        '''
+        Initialize the request handler.
+        '''
+        super().__init__(request, client_address, server)
+
     def handle(self) -> None:
         '''
         Main serving logic. Overrides BaseRequestHandler's handle().
@@ -16,12 +29,8 @@ class PicoHTTPRequestHandler(socketserver.BaseRequestHandler):
             - Actual file getting served.
         '''
         self.connection: socket.socket = self.request
-        wfile = self.connection.makefile('wb', 2**16)
-        wfile.write(b'HTTP/1.1 200 OK\r\n')
-        wfile.write(b'Content-Type: text/html\r\n')
-        wfile.write(b'\r\n')
         with open('index.html', 'rb') as f:
-            wfile.write(f.read())
-
-        wfile.flush()
-        wfile.close()
+            body = f.read()
+        headers = f'HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nContent-Length: {len(body)}\r\nConnection: close\r\n\r\n'
+        response = headers.encode() + body
+        self.connection.sendall(response)
